5d8900b081c21dc81a137802751f5eda6415c7ea
Guillaume LARIVIERE <glarivie@e3r11p5.42.fr>
2016-10-09 20:35:41 +0200

    build fix

--- pages/_index.md
+++ pages/_index.md
@@ -1,3 +1,12 @@
 ---
 title: "Apprendre JavaScript et React en français"
 ---
+
+sfsdfsdfsdfs sfsdf sdfsdfsd s s fsdf sdfs dfsd  fsdf sf sd fsdfsdfsd
+sdfsdfsdfsdf
+ad
+
+# afadasd
+## afafadada
+
+asfsdfsdfsdfsdfsdf
--- pages/docs/_template.jsx
+++ pages/docs/_template.jsx
@@ -1,75 +1,28 @@
 import React, { PropTypes, Component } from 'react';
-import { Link } from 'react-router';
-import Breakpoint from 'components/Breakpoint';
-import find from 'lodash/find';
-import { prefixLink } from 'gatsby-helpers';
-import { config } from 'config';
-import _ from 'lodash';
+// import { Link } from 'react-router';
+// import { prefixLink } from 'gatsby-helpers';
+// import { config } from 'config';
+// import _ from 'lodash';
 
 // import 'css/Docs.scss';
 
 class Docs extends Component {
   static contextTypes = {
-    router: React.PropTypes.object.isRequired,
+    router: PropTypes.object.isRequired,
   };
 
   static propTypes = {
     children: PropTypes.object,
     route: PropTypes.object,
+    location: PropTypes.object,
   };
 
-  handleTopicChange = ({ target : { value } }) => this.context.router.push(value);
+  handleTopicChange = ({ target: { value } }) => this.context.router.push(value);
 
   render() {
-    const { route: { pages }, children, location: { pathname } } = this.props;
-    const childPages = config.docPages.map((p) => {
-      const { data: { title }, path } = find(pages, (_p) => _p.path === p);
-      return ({ title, path });
-    });
-    const docOptions = childPages.map(({ path, title }) => (
-      <option
-        key={prefixLink(path)}
-        value={prefixLink(path)}
-      >
-        {title}
-      </option>
-    ));
-    const docPages = childPages.map(({ path, title }, index) => (
-      <li
-        key={index}
-        className="list"
-      >
-        <Link to={prefixLink(path)}>
-          {_.isEqual(prefixLink(path), pathname) ? <strong>{title}</strong> : title}
-        </Link>
-      </li>
-    ));
-
     return (
       <div>
-        <Breakpoint mobile>
-          <nav>
-            <ul>
-              {docPages}
-            </ul>
-          </nav>
-          <div>
-            {children}
-          </div>
-        </Breakpoint>
-        <Breakpoint>
-          <strong>Topics:</strong>
-          {' '}
-          <select
-            defaultValue={pathname}
-            onChange={this.handleTopicChange}
-          >
-            {docOptions}
-          </select>
-          <br />
-          <br />
-          {children}
-        </Breakpoint>
+        {this.props.children}
       </div>
     );
   }
