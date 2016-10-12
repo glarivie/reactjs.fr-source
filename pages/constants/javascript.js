7d6205fe1e1be638bb11b4e5db51ef838d50fac9
hqro <gui.lariviere@gmail.com>
2016-10-12 23:07:54 +0200

    JavaScript menu

--- pages/constants/javascript.js
+++ pages/constants/javascript.js
@@ -1,14 +1,92 @@
 const menu = [
   {
-    name: 'Introduction',
+    name: 'Les bases du JavaScript',
     children: [
       {
-        title: 'Les bases du JavaScript',
-        url: 'bases-javascript-es5.html',
+        title: 'Introduction',
+        url: 'introduction-es5.html',
       },
       {
-        title: 'Javascript ES6/ES2015 en 10 minutes',
-        url: 'javascript-es6-es2015-10-minutes.html',
+        title: 'Types',
+        url: 'types.html',
+      },
+      {
+        title: 'Variables',
+        url: 'variables.html',
+      },
+      {
+        title: 'Boucles et conditions',
+        url: 'boucles-conditions.html',
+      },
+      {
+        title: 'Gestion des erreurs',
+        url: 'gestion-erreurs-try-catch.html',
+      },
+      {
+        title: 'Fonctions',
+        url: 'fonctions-es5.html',
+      },
+    ],
+  },
+  {
+    name: 'Le JavaScript ES6+',
+    children: [
+      {
+        title: 'Introduction',
+        url: 'introduction-es6-es2015.html',
+      },
+      {
+        title: 'Let, Const',
+        url: 'variables-let-const.html',
+      },
+      {
+        title: 'Chaines de caracteres',
+        url: 'chaines-caracteres.html',
+      },
+      {
+        title: 'Fonctions flechees',
+        url: 'boucles-conditions.html',
+      },
+      {
+        title: 'Classes',
+        url: 'classes.html',
+      },
+      {
+        title: 'Destructuration',
+        url: 'destructuration.html',
+      },
+      {
+        title: 'Rest, spread...',
+        url: 'rest-spread-es6.html',
+      },
+      {
+        title: 'Boucles for-of',
+        url: 'boucles-for-of.html',
+      },
+      {
+        title: 'Async / Await',
+        url: 'async-await.html',
+      },
+      {
+        title: 'Import, export',
+        url: 'modules-import-export.html',
+      },
+    ],
+  },
+  {
+    name: 'L\'ecosysteme JavaScript',
+    children: [
+      {
+        title: 'Node package manager',
+        url: 'npm.html',
+      },
+      {
+        title: 'Babel',
+        url: 'babel-js.html',
+      },
+      {
+        title: 'Lodash',
+        url: 'lodash.html',
       },
     ],
   },
