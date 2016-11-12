36c3071bfa6d327e190ab5073dfb37d5d0381d15
hqro <gui.lariviere@gmail.com>
2016-11-12 20:12:32 +0100

    Add newsletter subscrition

--- components/Newsletter/NewsletterInput.js
+++ components/Newsletter/NewsletterInput.js
@@ -1,4 +1,5 @@
 import React, { PropTypes, Component } from 'react';
+import cx from 'classnames';
 
 // Styles
 import styles from './NewsletterInput.module.scss';
@@ -21,16 +22,29 @@ class NewsletterInput extends Component {
     const { emailInput } = this.state;
 
     return (
-      <form className={styles.newsletterInput}>
+      <form
+        className={cx(styles.newsletterInput, 'validate')}
+        action="//reactjs.us14.list-manage.com/subscribe/post?u=2aff359dbf02bb1176d572882&amp;id=9c62b0f794"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        target="_blank"
+        noValidate
+      >
         <input
+          className="required email"
+          id="mce-EMAIL"
           onChange={this.handleChange}
           value={emailInput}
           type="email"
-          name="email"
+          name="EMAIL"
           placeholder={placeholder}
           required
         />
-        <button type="submit" style={{ backgroundColor: buttonColor }}>
+        <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+          <input type="text" name="b_2aff359dbf02bb1176d572882_9c62b0f794" tabIndex="-1" value="" />
+        </div>
+        <button type="submit" style={{ backgroundColor: buttonColor }} id="mc-embedded-subscribe">
           {buttonText}
         </button>
       </form>
